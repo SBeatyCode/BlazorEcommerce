1a98e5fb1d59c560df0094156b0c0a7442df0908
Stephen Beaty <31738463+SBeatyCode@users.noreply.github.com>
2024-01-25 14:19:36 -0500

    Added 'Blazored Local Storage" 3rd party Library to use with the upcoming Cart

--- Client/Program.cs
+++ Client/Program.cs
@@ -1,6 +1,7 @@
 using BlazorEcommerce.Client;
 using BlazorEcommerce.Client.Services.CategoryService;
 using BlazorEcommerce.Client.Services.ProductServices;
+using Blazored.LocalStorage;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
@@ -8,6 +9,7 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
+builder.Services.AddBlazoredLocalStorage();
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddScoped<IProductService, ProductService>();
 builder.Services.AddScoped<ICategoryService, CategoryService>();
